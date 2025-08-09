44135a2f4770b3ef45b71bd38072edaa5793ac91
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-08-09 17:36:47 -0400

    feat: Site header now provides home page return link

--- spa-records-app/src/app/header/header.ts
+++ spa-records-app/src/app/header/header.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavMenu } from '../nav-menu/nav-menu';
+import { RouterLink } from '@angular/router';
 
 @Component( {
   selector: 'app-header',
   templateUrl: './header.html',
   imports: [
-    NavMenu
+    NavMenu,
+    RouterLink
   ],
   styleUrl: './header.scss'
 })
