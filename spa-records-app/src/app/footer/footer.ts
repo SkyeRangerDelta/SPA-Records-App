4d6104a22736947f14d027589e13545fc33d972a
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-08-09 00:08:54 -0400

    feat: Added external footer links

--- spa-records-app/src/app/footer/footer.ts
+++ spa-records-app/src/app/footer/footer.ts
@@ -7,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrl: './footer.scss'
 })
 export class Footer {
+  baseLink = 'https://sarimportauthority.org';
+  footerALinks = [
+    { label: 'About the Authority', url: `${this.baseLink}/about` },
+    { label: 'Regulations', url: `${this.baseLink}/regulations` },
+    { label: 'Public Notices', url: `${this.baseLink}/notices` }
+  ];
 
+  footerBLinks = [
+    { label: 'Contact Us', url: `${this.baseLink}/contact` },
+    { label: 'Emergency Procedures', url: `${this.baseLink}/eprocs` },
+    { label: 'Harbour Services', url: `${this.baseLink}/harbourservices` }
+  ];
 }
