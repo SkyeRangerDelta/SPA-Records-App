1c40f4a12907a4251fa610088acbfaec7673fb8e
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-08-09 00:20:18 -0400

    feat: Added the intro

--- spa-records-app/src/app/app.routes.ts
+++ spa-records-app/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { Intro } from './intro/intro';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Intro }
+];
