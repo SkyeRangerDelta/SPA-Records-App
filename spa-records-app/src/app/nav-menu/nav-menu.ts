7a06fb840381ebb881167c3f3b54c439a1e7474b
SkyeRangerDelta <chessmasterhutchison@gmail.com>
2025-08-08 23:23:23 -0400

    feat: Added nav content

--- spa-records-app/src/app/nav-menu/nav-menu.ts
+++ spa-records-app/src/app/nav-menu/nav-menu.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav-menu',
-  imports: [],
+  imports: [
+    RouterLinkActive,
+    RouterLink
+  ],
   templateUrl: './nav-menu.html',
   styleUrl: './nav-menu.scss'
 })
 export class NavMenu {
-
 }
